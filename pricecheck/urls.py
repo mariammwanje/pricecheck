c11ee8f322ada57dbd5872c4ce33be0fcb9a03c4
mariammwanje <mariamnanono498gmail.com>
2017-05-11 15:07:30 +0300

    Addition of Products App , module for product creation and  upload of pictures from database

--- pricecheck/urls.py
+++ pricecheck/urls.py
@@ -1,4 +1,4 @@
-"""pricecheck URL Configuration
+"""price_management URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.11/topics/http/urls/
@@ -13,9 +13,23 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
+from django.conf.urls.static import static
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+from price_management import settings
+from price_management import views
+from price_management.views import HomeView
 
 urlpatterns = [
+    # url(r'^$', views.home, name='home'),
+    url(r'^$', HomeView.as_view(), name='home'),
     url(r'^admin/', admin.site.urls),
+    url(r'^products/', include('products.urls', namespace='products')),
+    # url(r'^sellers/', include('sellers.urls', namespace='sellers')),
+    # #url(r'^accounts/', include('accounts.urls', namespace='accounts')),
 ]
+urlpatterns += staticfiles_urlpatterns()
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
