a80cfff3934c7393804121640785d35ec06b406d
mariammwanje <mariamnanono498gmail.com>
2017-05-11 15:25:28 +0300

    Addition of pricecheck database

--- pricecheck/settings.py
+++ pricecheck/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'products',
-    # 'accounts',
+    'accounts',
     # 'sellers',
     'bootstrap3',
     # 'django.contrib.sites',
@@ -76,7 +76,7 @@ TEMPLATES = [
         },
     },
 ]
-WSGI_APPLICATION = 'price_management.wsgi.application'
+WSGI_APPLICATION = 'pricecheck.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
